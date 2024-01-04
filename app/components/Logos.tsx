72ee8f7eea63dda5b6d7fd5cea25293f8d45e771
Allen Larson <127446190+allenlarson@users.noreply.github.com>
2024-01-04 13:31:32 -0500

    design changes to logos

--- app/components/Logos.tsx
+++ app/components/Logos.tsx
@@ -37,7 +37,7 @@ const Logos = () => {
   ];
 
   return (
-    <div className="flex flex-row flex-wrap contain md:gap-4 justify-center mt-20 md:mt-40">
+    <div className="flex flex-row flex-wrap container md:gap-4 justify-center mt-20 md:mt-40  p-4 bg-slate-500/10 rounded-xl">
       {logos.map(logo => (
         <span
           className="flex w-1/2 md:w-1/5 p-1 md:p-2 rounded-md hover:bg-slate-500/10 justify-center"
