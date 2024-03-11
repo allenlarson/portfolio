07d43d84a24dfa5e67d24af63d0e5259c029796f
Allen Larson <127446190+allenlarson@users.noreply.github.com>
2024-03-11 12:17:30 -0400

    added Porfolio data

---
+++ data/Portfolio.tsx
@@ -0,0 +1,56 @@
+export const Porfolio = [
+  {
+    title: 'RHA Collection',
+    description: '',
+    url: 'https://rhacollection.com/',
+    image: '',
+  },
+  {
+    title: 'Revance Aesthetics',
+    description: '',
+    url: 'https://revanceaesthetics.com/',
+    image: '',
+  },
+  {
+    title: 'Merz Xperience',
+    description: '',
+    url: 'https://www.xperiencemerz.com/',
+    image: '',
+  },
+  {
+    title: 'Revance Corporate Website',
+    description: '',
+    url: 'https://revance.com/',
+    image: '',
+  },
+  {
+    title: 'Revance Aesthetics',
+    description: '',
+    url: 'https://revanceaesthetics.com/',
+    image: '',
+  },
+  {
+    title: 'Kingsmen Digital Ventures',
+    description: '',
+    url: 'https://kingsmendv.com/',
+    image: '',
+  },
+  {
+    title: 'MiraDry',
+    description: '',
+    url: 'https://www.miradry.com/',
+    image: '',
+  },
+  {
+    title: 'Growchow Law',
+    description: '',
+    url: 'https://grochowlaw.com/',
+    image: '',
+  },
+  {
+    title: 'Ferguson & May',
+    description: '',
+    url: 'https://fergusonmay.com/',
+    image: '',
+  },
+];
